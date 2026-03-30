53a96f8497b711df02c97260a6295f073a36389d
YcaroJesus <ycaro.silva3@etec.sp.gov.br>
2026-03-30 03:17:32 -0300

    Add files via upload

---
+++ Atividades/Ex2/2.OlaNome/2.OlaNome/olaNome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.OlaNome
+{
+    internal class Nome
+    {
+        public void classe()
+        {
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá {0}", nome);
+        }
+    }
+}
---
+++ Atividades/Ex4/4.Media3/4.Media3/Media.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Media3
+{
+    internal class Media
+    {
+        public float Calculo(int n1,int n2,int n3) 
+        {
+            float resultado = (n1 + n2 + n3) / 3;
+            return resultado;
+        }
+    }
+}
